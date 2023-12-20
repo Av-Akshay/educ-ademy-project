5bb61de7ceb30129303dfea353bbf7ca42cf9830
Av-Akshay <akshuchauhan1jan@gmail.com>
2023-12-20 10:22:35 -0800

    feat: Navbar is comleted

--- src/components/header/NavBar.jsx
+++ src/components/header/NavBar.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const NavBar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="flex w-screen h-[5vh] items-center justify-between">
+      <div className="w-3/12 text-center"> Lorem IPSUM</div>
+      <div className="w-9/12 text-right pr-[10rem]">Link</div>
+    </div>
+  );
 };
 
 export default NavBar;
