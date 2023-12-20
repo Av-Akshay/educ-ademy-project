11f6a976c50986adf36c410945ebfc2bb3c463e9
Av-Akshay <akshuchauhan1jan@gmail.com>
2023-12-20 09:29:16 -0800

    chore: telwind css setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import NavBar from "./components/header/NavBar";
+import CenterSection from "./components/center-section/CenterSection";
+import Footer from "./components/footer/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <CenterSection />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
