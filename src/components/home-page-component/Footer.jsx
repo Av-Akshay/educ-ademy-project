0fdb3f9775127b95ccf201ee5cf5aeaa615a7d24
Av-Akshay <akshuchauhan1jan@gmail.com>
2023-12-23 14:18:19 +0530

    style: make footer component responsive

--- src/components/home-page-component/Footer.jsx
+++ src/components/home-page-component/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   const date = new Date().toLocaleDateString();
   // console.log(date);
   return (
-    <div className="bg-slate-900 h-[5vh] flex items-center text-white pl-24">
+    <div className="bg-slate-900 h-[5vh] flex items-center text-white pl-24 max-md:pl-14 max-sm:pl-4 max-sm:text-sm">
       <p> © {date} Akshay Chauhan. All Right Reserved | Term and conditions</p>
     </div>
   );
